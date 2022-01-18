ef6a7b19b74194976d464bf3239aa77026247530
Kristian Lein-Mathisen <kristianlein@gmail.com>
2022-01-18 15:27:24 +0100

    port: benchmark example
    rocksdb leftovers

--- tests/benchmark.scm
+++ tests/benchmark.scm
@@ -1,14 +1,14 @@
-;;; inserts a lot of tiny entries into benchmark.rocks as quickly as
+;;; inserts a lot of tiny entries into benchmark.level as quickly as
 ;;; possible.
 ;;;
 ;;; csi -s tests/benchmark.scm | pv -l >/dev/null
 ;;;
 ;;; I'm getting ~500k/s, you? #:wal seems to have little effect, bug?
 ;;;
-(import rocksdb chicken.string)
+(import leveldb chicken.string)
 
-(define db (rocksdb-open "benchmark.rocks"))
-(define wb (rocksdb-writebatch))
+(define db (leveldb-open "benchmark.ldb" max-file-size: (* 32 1024 1024)))
+(define wb (leveldb-writebatch))
 
 (define commit!
   (let ((count 0))
@@ -16,12 +16,12 @@
       (set! count (+ 1 count))
       (when (or force? (>= count 1000))
         (set! count 0)
-        (rocksdb-write db wb sync: #f wal: #f)
-        (rocksdb-writebatch-clear wb)))))
+        (leveldb-write db wb sync: #f)
+        (leveldb-writebatch-clear wb)))))
 
 (let loop ((n 10000000))
   (when (> n 0)
-    (rocksdb-writebatch-put wb (conc "n" n) (conc "v" n))
+    (leveldb-writebatch-put wb (conc "k" n) (conc "v" n))
     (commit! #f)
     (print n)
     (loop (- n 1))))
