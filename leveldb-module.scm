7df59d951937232c08e4bbde989bd7ca6bfdcd64
Kristian Lein-Mathisen <kristianlein@gmail.com>
2022-01-19 16:48:45 +0100

    cleanup: fix indentation

--- leveldb-module.scm
+++ leveldb-module.scm
@@ -1,31 +1,32 @@
-(module leveldb (leveldb-open leveldb-close
-                              leveldb-version
-                              leveldb-t           leveldb-t?
+(module leveldb (leveldb-version
+                 leveldb-open leveldb-close
+                 leveldb-version
+                 leveldb-t           leveldb-t?
 
-                              leveldb-get leveldb-put leveldb-delete
+                 leveldb-get leveldb-put leveldb-delete
 
-                              leveldb-iterator-t  leveldb-iterator-t?
-                              leveldb-iterator
-                              leveldb-iter-valid?
-                              leveldb-iter-seek
-                              leveldb-iter-next             leveldb-iter-prev
-                              leveldb-iter-key              leveldb-iter-value
-                              leveldb-iter-destroy
+                 leveldb-iterator-t  leveldb-iterator-t?
+                 leveldb-iterator
+                 leveldb-iter-valid?
+                 leveldb-iter-seek
+                 leveldb-iter-next             leveldb-iter-prev
+                 leveldb-iter-key              leveldb-iter-value
+                 leveldb-iter-destroy
 
-                              leveldb-writebatch-t?
-                              leveldb-writebatch
-                              leveldb-writebatch-put
-                              leveldb-writebatch-delete
-                              leveldb-writebatch-clear
-                              leveldb-writebatch-destroy
-                              leveldb-write
+                 leveldb-writebatch-t?
+                 leveldb-writebatch
+                 leveldb-writebatch-put
+                 leveldb-writebatch-delete
+                 leveldb-writebatch-clear
+                 leveldb-writebatch-destroy
+                 leveldb-write
 
-                              leveldb-compact-range
+                 leveldb-compact-range
 
-                              ;; ========== unofficial, in case I've got all this wrong:
-                              leveldb-iter-next*            leveldb-iter-prev*
-                              leveldb-iter-key*             leveldb-iter-value*
-                              leveldb-iter-seek*)
+                 ;; ========== unofficial, in case I've got all this wrong:
+                 leveldb-iter-next*            leveldb-iter-prev*
+                 leveldb-iter-key*             leveldb-iter-value*
+                 leveldb-iter-seek*)
 (import scheme chicken.base)
 (include "leveldb.scm")
 )
